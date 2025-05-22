fc1a23fec8a1f86cce3724fb73c9ee0aa3c68d98
drclcomputers <drclcomputers@gmail.com>
2025-05-22 10:44:54 +0300

    Added static routing for replit

--- replit.nix
+++ replit.nix
@@ -1,11 +1,11 @@
 { pkgs }: {
   deps = [
-    pkgs.go
     pkgs.python311
     pkgs.python311Packages.numpy
     pkgs.python311Packages.pandas
     pkgs.python311Packages.scikit-learn
     pkgs.python311Packages.joblib
     pkgs.sqlite
+    pkgs.go
   ];
 }
