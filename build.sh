03ed7714b8bf33841596e77464c2b8b9152f89cd
drclcomputers <132164125+drclcomputers@users.noreply.github.com>
2025-05-22 16:02:25 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ cd ..
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install --no-cache-dir -r requirements.txt
+pip install --no-cache-dir -r ./backend/requirements.txt
